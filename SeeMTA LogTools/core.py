e072dd03ee16c9a8c77940c327579cd70da1ac33
girannorbi <girannorbi2007@gmail.com>
2024-07-05 20:58:14 +0200

    Added support for opening logs and getting all the tickets.

---
+++ SeeMTA LogTools/core.py
@@ -0,0 +1,17 @@
+﻿class Main:
+    @staticmethod
+    def openFile(file_path):
+        """
+        opens log file from the desired path.
+        Parameters:
+        fileDirectory (str, optional): the path to the desired file
+        Returns:
+        list:file content
+        """
+        file = open(file_path, "r", encoding="UTF-8")
+        content = file.readlines()
+        for i in range(len(content)):
+            content[i] = content[i].replace("\n" , "")
+        file.close()
+        return content
+
---
+++ SeeMTA LogTools/ticketcalculator.py
@@ -0,0 +1,39 @@
+﻿import re
+
+import core
+
+
+class Ticket:
+    def __init__(self, price, issuer, reason, date, log):
+        self.price = price
+        self.issuer = issuer
+        self.reason = reason
+        self.date = date
+        self.log = log
+
+
+def get_tickets(logfile):
+    """
+    gets all issued tickets found in the logfile
+    Paramters:
+        log: string
+    Returns:
+        array of all the found tickets
+    """
+    tickets = []
+    for i in range(len(logfile)):
+        if logfile[i].__contains__("[SeeMTA - Ticket]:"):
+            price = re.search("Bírság: (.*) Indok:", logfile[i]).group(1)
+            issuer = re.search(r"SeeMTA - Ticket]: (.*) megbüntette", logfile[i]).group(1)
+            reason = re.search("Indok: (.*)", logfile[i]).group(1)
+            date = re.search(r"\[(.*)\] \[Output\]", logfile[i]).group(1)
+            log = logfile[i]
+            tickets.append(Ticket(price, issuer, reason, date, log))
+    return tickets
+
+
+# for i in get_tickets(core.Main.openFile("C:/SeeMTA/mta/logs/console-2024-07-05.log")):
+#     print("\nTicket:")
+#     print(i.issuer)
+#     print(i.price)
+#     print(i.reason)
