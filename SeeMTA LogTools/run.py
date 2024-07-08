d2195a95758b0d6177cee8eef519ca108ba5edfc
girannorbi <girannorbi2007@gmail.com>
2024-07-08 21:18:22 +0200

    Mostmár fut realtime a program, kiírja a büntetéseket, filedialog implementálva

---
+++ SeeMTA LogTools/run.py
@@ -0,0 +1,35 @@
+﻿import os
+import re
+import core
+from  ticketcalculator import calc_all_tickets, get_tickets, get_last_ticket
+import time
+import tkinter as tk
+from tkinter import filedialog
+
+root = tk.Tk()
+root.withdraw()
+
+file_path = filedialog.askopenfile("r").name
+
+
+chosen_issuer = input("Kinek a csekkjeit szeretnéd mérni?\n")
+os.system("cls")
+
+while True:
+    log = core.read_log_file(file_path)
+    price_total, ticket_ammount, price_average = calc_all_tickets(get_tickets(log), chosen_issuer)
+    last_issued, last_price, last_reason = get_last_ticket(get_tickets(log), chosen_issuer)
+    print(f"Kiválasztott név: {chosen_issuer}")
+    if price_total != 0 and ticket_ammount != 0 and chosen_issuer != 0:
+        print(f"\n"
+              f"Összesen: {price_total}$\n"
+              f"Csekkek darabszáma: {ticket_ammount}db\n"
+              f"Átlag büntetés: {price_average}"
+              f"\n\n")
+        if last_issued:
+            print(f"Legutolsó büntetés:\n"
+                  f"Büntetett neve: {last_issued}\n"
+                  f"Büntetés összege: {last_price}$\n"
+                  f"Büntetés oka: {last_reason}")
+    time.sleep(15)
+    os.system('cls')
--- SeeMTA LogTools/ticketcalculator.py
+++ SeeMTA LogTools/ticketcalculator.py
@@ -1,15 +1,18 @@
-﻿import re
+﻿import os
+import re
+import time
 
 import core
 
 
 class Ticket:
-    def __init__(self, price, issuer, reason, date, log):
+    def __init__(self, price, issuer, reason, date, log, issued):
         self.price = price
         self.issuer = issuer
         self.reason = reason
         self.date = date
         self.log = log
+        self.issued = issued
 
 
 def get_tickets(logfile):
@@ -23,22 +26,35 @@ def get_tickets(logfile):
     tickets = []
     for i in range(len(logfile)):
         if logfile[i].__contains__("[SeeMTA - Ticket]:"):
-            price = re.search("Bírság: (.*) Indok:", logfile[i]).group(1)
+            price = int(re.search("Bírság: (.*) Indok:", logfile[i]).group(1).replace(" ", "").replace("$", ""))
             issuer = re.search(r"SeeMTA - Ticket]: (.*) megbüntette", logfile[i]).group(1)
+            issued = re.search(r"megbüntette (.*) játékost", logfile[i]).group(1)
             reason = re.search("Indok: (.*)", logfile[i]).group(1)
             date = re.search(r"\[(.*)\] \[Output\]", logfile[i]).group(1)
             log = logfile[i]
-            tickets.append(Ticket(price, issuer, reason, date, log))
+            tickets.append(Ticket(price, issuer, reason, date, log, issued))
     return tickets
 
+def calc_all_tickets(tickets, chosen_issuer):
+    price_total = 0
+    ticket_amount = 0
+    price_average = 0
+    if len(tickets) != 0:
+        for i in tickets:
+            if i.issuer == chosen_issuer:
+                price_total += i.price
+                ticket_amount += 1
+        price_average = price_total/ticket_amount
+    return price_total, ticket_amount, price_average
 
-files = ["C:/SeeMTA/mta/logs/console-2024-07-05.log", "C:/SeeMTA/mta/logs/console-2024-07-04.log", "C:/SeeMTA/mta/logs/console-2024-07-03.log", "C:/SeeMTA/mta/logs/console-2024-07-01.log"]
-log = core.read_log_file(files)
-tickets = get_tickets(log)
-for i in tickets:
-    print("")
-    print(i.issuer)
-    print(i.reason)
-    print(i.price)
-print("\nÖsszesen:")
-print(str(len(tickets)) + " darab")
+def get_last_ticket(tickets, chosen_issuer):
+    if len(tickets) == 0:
+        return None
+    last_ticket = None
+    for current_ticket in tickets:
+        if(current_ticket.issuer == chosen_issuer):
+            last_ticket = current_ticket
+    if isinstance(last_ticket, Ticket):
+        return last_ticket.issued, last_ticket.price, last_ticket.reason 
+    else:
+        return None
